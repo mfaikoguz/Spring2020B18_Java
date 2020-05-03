320d0ba2f73475aa679352ed5b3d890b96297a75
Your Name <mfaikoguz@gmail.com>
2020-05-03 01:30:08 -0500

    first push

---
+++ src/Practice/Assessment_test_5_2_Odd_Numbers.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Assessment_test_5_2_Odd_Numbers {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("#1: ");
+        int num1 = s.nextInt();
+        System.out.print("#2: ");
+        int num2 = s.nextInt();
+
+        
+    }
+}
