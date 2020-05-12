f462c7720f3cd4fa2bae9e101f71653715940606
Your Name <mfaikoguz@gmail.com>
2020-05-12 11:16:51 -0500

    day38

--- src/day38_Constructors/CircleObject.java
+++ src/day38_Constructors/CircleObject.java
@@ -1,16 +1,23 @@
 package day38_Constructors;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class CircleObject {
+
     public static void main(String[] args) {
-        double r = 3;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle ");
+        double r = scan.nextDouble();
 
         Circle circle1 = new Circle(r);
 
         double areaOfCircle = circle1.area();
-
-        System.out.println(areaOfCircle);
-
-        System.out.println(circle1);
+        System.out.println("Diameter is: " + circle1.diameter);
+        System.out.println("Perimeter is: " + circle1.perimeter());
+        System.out.println("Area is: " + df.format(areaOfCircle));
 
     }
 }
--- src/day38_Constructors/Item.java
+++ src/day38_Constructors/Item.java
@@ -1,17 +1,16 @@
 package day38_Constructors;
+/*
+ create a class called Item
+                instance variables:
+                    name, unitPrice, quantity
+                add a constructor that can initialize the fields
+                instance methods:
+                    calcCost(): returns the total cost as double
+                                hint: quantity * unitPrice
+                    toString(): returns the name, unit price, quantity and total cost info as calculated by calcCost()
+ */
 
 public class Item {
-    /*
-    create a class called Item
-			instance variables:
-				name, unitPrice, quantity
-			add a constructor that can initialize the fields
-			instance methods:
-				calcCost(): returns the total cost as double
-							hint: quantity * unitPrice
-				toString(): returns the name, unit price, quantity and total cost info as calculated by calcCost()
-
-     */
 
     String name;
     double unitPrice;
@@ -19,10 +18,19 @@ public class Item {
 
     public Item(String name, double unitPrice, int quantity) {
         this.name = name;
-        this.unitPrice = unitPrice;
         this.quantity = quantity;
+        this.unitPrice = unitPrice;
     }
 
+
+    public double calcCost() {
+        return quantity * unitPrice;
+    }
+
+    public String toString() {
+        return "Item: " + name + ", Unit price: $" + unitPrice + ", Quantity: " + quantity +
+                "\nTotal Cost of Item: $" + calcCost();
+    }
 
 
 }
