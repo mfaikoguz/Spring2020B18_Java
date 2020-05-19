5eda314d1cad3fda8f95593aab55d844e963f2d4
Your Name <mfaikoguz@gmail.com>
2020-05-19 14:48:08 -0500

    day42

--- src/day42_Inheritance/Constructor2.java
+++ src/day42_Inheritance/Constructor2.java
@@ -1,14 +1,25 @@
 package day42_Inheritance;
 
 class Test {
-    public Test() {
+    public Test(int a) {
         System.out.println("A");
     }
 }
 
 
-public class Constructor2 {
+public class Constructor2 extends Test {
+
+
     public Constructor2() {
+        super(15);
         System.out.println("B");
     }
+
+    public static void main(String[] args) {
+        // Test obj = new Test(10); // A
+
+        Constructor2 obj2 = new Constructor2(); // A B
+    }
+
+
 }
