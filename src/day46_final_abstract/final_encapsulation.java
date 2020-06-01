1688866a43cd8f2f6959cd5e4877ce59887e4db8
Your Name <mfaikoguz@gmail.com>
2020-06-01 09:45:22 -0500

    day46

--- src/day46_final_abstract/final_encapsulation.java
+++ src/day46_final_abstract/final_encapsulation.java
@@ -1,16 +1,19 @@
 package day46_final_abstract;
 
 public class final_encapsulation {
-    private int data1 = 200;
+    final private int data1 = 200;
     private int data2 = 300;
 
     public int getData1() {
         return data1;
     }
+/*
 
     public void setData1(int data1) {
-        this.data1 = data1;
+        this.data1 = data1; // cannot assign a new data to a finalized variable
     }
+
+*/
 
     public int getData2() {
         return data2;
