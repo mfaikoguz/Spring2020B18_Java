9ce7c618ff628f25ac34faf4511456d22a37caa7
Your Name <mfaikoguz@gmail.com>
2020-06-01 09:48:46 -0500

    day46

---
+++ src/day46_final_abstract/credentials.java
@@ -0,0 +1,15 @@
+package day46_final_abstract;
+
+public class credentials {
+
+    final private String username = "Cybertek";
+    final private String password = "cybertek12345";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
