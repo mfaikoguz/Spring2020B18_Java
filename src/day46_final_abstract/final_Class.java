9afcbd2fb0151051f8be13e4a1e388e3b5fe0f6e
Your Name <mfaikoguz@gmail.com>
2020-06-01 13:51:50 -0500

    day46

---
+++ src/day46_final_abstract/final_Class.java
@@ -0,0 +1,24 @@
+package day46_final_abstract;
+
+class Data {
+
+}
+
+public final class final_Class extends Data { // doesn't compile because final class can't be the super class
+
+    /*
+    final {
+
+    }
+
+    final static{
+
+    }
+    */
+
+
+}
+
+
+
+// class Data2 extends final_Class { }
---
+++ src/day46_final_abstract/final_Method.java
@@ -0,0 +1,20 @@
+package day46_final_abstract;
+
+public class final_Method {
+    public final void method1() {
+
+    }
+
+    public final void method1(int a) {
+
+    }
+
+    public final static void main(String[] args) {
+
+    }
+
+    /* public final final_Method() {
+
+    }
+    */
+}
