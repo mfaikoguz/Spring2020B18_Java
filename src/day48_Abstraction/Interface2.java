d18828c958832b5bf5672c5d2a8baa308af85394
Your Name <mfaikoguz@gmail.com>
2020-06-08 14:48:00 -0500

    day48

--- src/day48_Abstraction/Interface2.java
+++ src/day48_Abstraction/Interface2.java
@@ -2,10 +2,24 @@ package day48_Abstraction;
 
 public interface Interface2 {
 
+    // public Interface2(){}
+
+    // {}
+
+    // public void method1(){}
+
     int a = 100;
 
     public static void main(String[] args) {
         System.out.println(a); // by default static
         // a = 200; // by default final
+        System.out.println(Interface2.a);
+        Interface2.method4();
     }
+
+    public static void method4(){ }
+
+    void method5();
+
+
 }
