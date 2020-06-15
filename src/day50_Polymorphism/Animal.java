a9fbaaf11e468278f9e3fa9191ea2decd164d1b3
Your Name <mfaikoguz@gmail.com>
2020-06-15 13:19:41 -0500

    day50

--- src/day50_Polymorphism/Animal.java
+++ src/day50_Polymorphism/Animal.java
@@ -1,35 +1,27 @@
 package day50_Polymorphism;
 
-public class Animal {
-    /*
-    Warmup tasks:
-    1. cerate a class Animal
-            variables: age, gender
-            methods: eat, sleep
-        DO NOT make abstract we need this for today's example
-    2. create subclass of Animal named Cat
-            variable: catName
-            methods: scratch
-    3. create subclass of Animal named Dog
-            variable: dogName
-            methods: bark
-     */
+public abstract class Animal { // 2 var, 2 in method
+
     public int age;
     public char gender;
 
     public void eat() {
-        System.out.println("eating...");
+        System.out.println("Animal is eating");
     }
 
     public void sleep() {
-        System.out.println("sleeping...");
+        System.out.println("Animal is sleeping");
+    }
+
+    public static void methodA() {
+        System.out.println("Animal Class");
     }
 }
 
 final class Cat extends Animal {
     public String catName;
 
-    public Cat(String catName, char gender, int age) {
+    public Cat(String catName, char gender, int age) { // 3 variables 3 methods
         this.catName = catName;
         this.gender = gender;
         this.age = age;
@@ -50,10 +42,29 @@ final class Cat extends Animal {
     }
 }
 
-class Dog extends Animal {
+final class Dog extends Animal { // 3 var, 3 methods
     public String dogName;
 
+    public Dog(String dogName, int age, char gender) {
+        this.dogName = dogName;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public void eat() {
+        System.out.println("Dog " + dogName + " is eating");
+    }
+
+    public void sleep() {
+        System.out.println("Dog " + dogName + " is sleeping");
+    }
+
     public void bark() {
-        System.out.println(dogName + " is barking...");
+        System.out.println("Dog " + dogName + " is barking");
+    }
+
+
+    public static void methodA(int a) {
+        System.out.println("Dog Class A");
     }
 }
